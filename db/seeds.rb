1f4a50e396444b2825a927de75764fc7bf46034a
Sambath Prum <psb.sambath@gmail.com>
2014-09-29 11:48:35 -0500

    Added association between Post and User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 require 'faker'
 
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 50.times do
   Post.create!(
+    user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -15,6 +27,12 @@ posts = Post.all
   )
 end
 
+users.first.update_attributes!(
+  email:    'psb.sambath@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
