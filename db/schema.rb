d7558f4beed60da3a745449c2ef814150915eb0c
Sambath Prum <psb.sambath@gmail.com>
2014-10-06 12:34:32 -0500

    Added avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001143226) do
+ActiveRecord::Schema.define(version: 20141006150517) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20141001143226) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
